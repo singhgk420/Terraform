40674d76619c8b46a9926133636478514865bb67
singhgk420 <73923943+singhgk420@users.noreply.github.com>
2021-07-18 20:28:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-1"
+   shared_credentials_file = "/Users/Gaurav/.aws/credentials"
+   profile = "testing"
 }
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
